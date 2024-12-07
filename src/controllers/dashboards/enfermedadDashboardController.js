fa75fbe29428ba8fc627108de24d983b932631a9
santiago <santiagomejiagar@gmail.com>
2024-12-06 19:57:35 -0500

    se crean servicios, controladores y dash

--- src/controllers/dashboards/enfermedadDashboardController.js
+++ src/controllers/dashboards/enfermedadDashboardController.js
@@ -1,12 +1,11 @@
-import { buscarMedicamentos} from "../../services/medicamentoService.js"
-
-buscarMedicamentos()
+import { buscarEnfermedades } from "../../services/enfermedadService.js"
+buscarEnfermedades()
 .then(function(respuestaBack){
     console.log(respuestaBack)
     let fila = document.getElementById("fila")
 
-    respuestaBack.forEach(function(medicamento){
-    console.log(medicamento)
+    respuestaBack.forEach(function(enfermedad){
+    console.log(enfermedad)
     let columna = document.createElement("div")
     columna.classList.add("col")
 
@@ -14,8 +13,8 @@ buscarMedicamentos()
     tarjeta.classList.add("card", "p-5", "h-100", "shadow")
 
     let nombre = document.createElement("h2")
-    nombre.textContent = medicamento.nombre
-    
+    nombre.textContent = enfermedad.nombre
+
     tarjeta.appendChild(nombre)
     columna.appendChild(tarjeta)
     fila.appendChild(columna)
