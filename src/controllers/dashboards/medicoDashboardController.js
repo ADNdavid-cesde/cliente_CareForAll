21587621881457dd1f116d195b180c221fee0b7a
ADNdavid <adn6b@hotmail.com>
2024-12-10 17:56:43 -0500

    Se organzan los dashboard de paciente y medico

--- src/controllers/dashboards/medicoDashboardController.js
+++ src/controllers/dashboards/medicoDashboardController.js
@@ -1,71 +1,98 @@
 import { traerMedicos } from "../../services/medicoService.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    traerMedicos()
-        .then(function (respuestaBack) {
-            console.log(respuestaBack)
-            mostrarTabla(respuestaBack);
-        })
-        .catch(function (error) {
-            console.error(error);
-        })
-})
+  let captionTabla = document.querySelector("caption");
+  traerMedicos()
+    .then(function (respuestaBack) {
+      console.log(respuestaBack);
+      mostrarTabla(respuestaBack);
+      if (respuestaBack.length != 0) {
+        captionTabla.innerHTML = "Da click en la fila para ver detalles";
+      } else {
+        captionTabla.innerHTML = "La tabla no cuenta con registros";
+      }
+    })
+    .catch(function (error) {
+      console.error(error);
+      captionTabla.innerHTML =
+        "Hubo un error al traer la información desde el servidor 😕";
+    });
+});
 
 function mostrarTabla(medicos) {
-    let cuerpoTabla = document.querySelector("table tbody");
-    cuerpoTabla.innerHTML = "";
+  let cuerpoTabla = document.querySelector("table tbody");
+  cuerpoTabla.innerHTML = "";
 
-    medicos.forEach((medico) => {
-        let fila = document.createElement("tr");
-        fila.setAttribute("data-bs-toggle", "modal");
-        fila.setAttribute("data-bs-target", "#ventanaModal");
-        fila.setAttribute("id", `med-${medico.id}`);
-        fila.innerHTML = `
+  medicos.forEach((medico) => {
+    let fila = document.createElement("tr");
+    fila.setAttribute("data-bs-toggle", "modal");
+    fila.setAttribute("data-bs-target", "#ventanaModal");
+    fila.setAttribute("id", `med-${medico.id}`);
+    fila.innerHTML = `
             <td> ${medico.nombre} </td>
             <td> ${medico.especialidad} </td>
             <td> ${medico.ips} </td>
             <td> ${medico.telefono} </td>
         `;
-        cuerpoTabla.appendChild(fila);
-    });
+    cuerpoTabla.appendChild(fila);
+  });
 }
 
 let cuerpoTabla = document.querySelector("table tbody");
 cuerpoTabla.addEventListener("click", (event) => {
-    console.log(event.target.parentElement.id)
-    if (event.target.parentElement.id.startsWith("med-")) {
-        mostrarMedicoModal(event.target.parentElement.id.slice(4));
-    }
-})
+  console.log(event.target.parentElement.id);
+  if (event.target.parentElement.id.startsWith("med-")) {
+    mostrarMedicoModal(event.target.parentElement.id.slice(4));
+  }
+});
 
 function mostrarMedicoModal(id) {
-    let nombreMedico=document.getElementById("nombremedico");
-    let matriculaProfesionalMedico=document.getElementById("matriculamedico");
-    let especialidadMedico=document.getElementById("especialidadmedico");
-    let salarioMedico=document.getElementById("salariomedico");
-    let ipsMedico=document.getElementById("ipsmedico");
-    let correoMedico=document.getElementById("correomedico");
-    let telefonoMedico=document.getElementById("telefonomedico");
-    let direccionMedico=document.getElementById("direccionmedico");
-    let disponibleFinDeSemanaMedico=document.getElementById("disponibilidadmedico");
+  let nombreMedico = document.getElementById("nombremedico");
+  let matriculaProfesionalMedico = document.getElementById("matriculamedico");
+  let especialidadMedico = document.getElementById("especialidadmedico");
+  let salarioMedico = document.getElementById("salariomedico");
+  let ipsMedico = document.getElementById("ipsmedico");
+  let correoMedico = document.getElementById("correomedico");
+  let telefonoMedico = document.getElementById("telefonomedico");
+  let direccionMedico = document.getElementById("direccionmedico");
+  let disponibleFinDeSemanaMedico = document.getElementById(
+    "disponibilidadmedico"
+  );
+  let listadoPacientes = document.getElementById("listaPacientes");
 
+  listadoPacientes.innerHTML = "";
 
-    traerMedicos()
-        .then(function (respuestaBack) {
-            console.log(respuestaBack);
-            let medico = respuestaBack.find((medico) => medico.id == id);
-            nombreMedico.textContent = medico.nombre;
-            especialidadMedico.value = medico.especialidad;
-            ipsMedico.value = medico.ips;
-            matriculaProfesionalMedico.value = medico.matriculaProfesional;
-            salarioMedico.value = medico.salario;
-            correoMedico.value = medico.correo;
-            telefonoMedico.value = medico.telefono;
-            direccionMedico.value = medico.direccion;
-            disponibleFinDeSemanaMedico.checked = medico.disponibleFinDeSemana;
-        
-        })
-        .catch(function (error) {
-            console.error(error);
-        })
+  traerMedicos()
+    .then(function (respuestaBack) {
+      console.log(respuestaBack);
+      let medico = respuestaBack.find((medico) => medico.id == id);
+      nombreMedico.textContent = medico.nombre;
+      especialidadMedico.value = medico.especialidad;
+      ipsMedico.value = medico.ips;
+      matriculaProfesionalMedico.value = medico.matriculaProfesional;
+      salarioMedico.value = medico.salario;
+      correoMedico.value = medico.correo;
+      telefonoMedico.value = medico.telefono;
+      direccionMedico.value = medico.direccionConsultorio;
+      disponibleFinDeSemanaMedico.checked = medico.finDeSemanaDisponible;
+
+      if (medico.pacientes.length != 0) {
+        medico.pacientes.forEach((paciente) => {
+          let item = document.createElement("li");
+          item.classList.add("list-group-item");
+          item.textContent = paciente.nombre;
+
+          listadoPacientes.appendChild(item);
+        });
+      } else {
+        let item = document.createElement("li");
+        item.classList.add("list-group-item");
+        item.textContent = "El medico no tiene pacientes a su cargo.";
+
+        listadoPacientes.appendChild(item);
+      }
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 }
---
+++ src/controllers/dashboards/pacienteDashboardController.js
@@ -0,0 +1,98 @@
+import { traerPacientes } from "../../services/pacienteService.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  let captionTabla = document.querySelector("caption");
+  traerPacientes()
+    .then(function (respuestaBack) {
+      console.log(respuestaBack);
+      mostrarTabla(respuestaBack);
+      if (respuestaBack.length != 0) {
+        captionTabla.innerHTML = "Da click en la fila para ver detalles";
+      } else {
+        captionTabla.innerHTML = "La tabla no cuenta con registros";
+      }
+    })
+    .catch(function (error) {
+      console.error(error);
+      captionTabla.innerHTML =
+        "Hubo un error al traer la información desde el servidor 😕";
+    });
+});
+
+function mostrarTabla(pacientes) {
+  let cuerpoTabla = document.querySelector("table tbody");
+  cuerpoTabla.innerHTML = "";
+
+  pacientes.forEach((paciente) => {
+    let fila = document.createElement("tr");
+    fila.setAttribute("data-bs-toggle", "modal");
+    fila.setAttribute("data-bs-target", "#ventanaModal");
+    fila.setAttribute("id", `pac-${paciente.id}`);
+    fila.innerHTML = `
+            <td> ${paciente.nombre} </td>
+            <td> ${paciente.ips} </td>
+            <td> ${paciente.grupoIngresos} </td>
+            <td> ${paciente.fechaAfiliacion} </td>
+        `;
+    cuerpoTabla.appendChild(fila);
+  });
+}
+
+let cuerpoTabla = document.querySelector("table tbody");
+cuerpoTabla.addEventListener("click", (event) => {
+  console.log(event.target.parentElement.id);
+  if (event.target.parentElement.id.startsWith("pac-")) {
+    mostrarPacienteModal(event.target.parentElement.id.slice(4));
+  }
+});
+
+function mostrarPacienteModal(id) {
+  let nombrePacienteH1 = document.getElementById("nombrePaciente");
+  let ciudadPacienteInput = document.getElementById("ciudadPaciente");
+  let telefonoPacienteInput = document.getElementById("telefonoPaciente");
+  let correoPacienteInput = document.getElementById("correoPaciente");
+  let grupoIngresosPacienteInput = document.getElementById(
+    "grupoIngresosPaciente"
+  );
+  let ipsPacienteInput = document.getElementById("ipsPaciente");
+  let nacimientoPacienteInput = document.getElementById("nacimientoPaciente");
+  let afiliacionPacienteInput = document.getElementById("afiliacionPaciente");
+  let polizaPacienteInput = document.getElementById("polizaPaciente");
+  let listadoSignosVitales = document.getElementById("listaSignosVitales");
+
+  listadoSignosVitales.innerHTML = "";
+
+  traerPacientes()
+    .then(function (respuestaBack) {
+      console.log(respuestaBack);
+      let paciente = respuestaBack.find((paciente) => paciente.id == id);
+      nombrePacienteH1.textContent = paciente.nombre;
+      ciudadPacienteInput.value = paciente.ciudad;
+      telefonoPacienteInput.value = paciente.telefono;
+      correoPacienteInput.value = paciente.correo;
+      grupoIngresosPacienteInput.value = paciente.grupoIngresos;
+      ipsPacienteInput.value = paciente.ips;
+      nacimientoPacienteInput.value = paciente.anioNacimiento;
+      afiliacionPacienteInput.value = paciente.fechaAfiliacion;
+      polizaPacienteInput.checked = paciente.poliza;
+
+      if (paciente.signosVitales.length != 0) {
+        paciente.signosVitales.forEach((paciente) => {
+          let item = document.createElement("li");
+          item.classList.add("list-group-item");
+          item.textContent = paciente.nombre;
+
+          listadoSignosVitales.appendChild(item);
+        });
+      } else {
+        let item = document.createElement("li");
+        item.classList.add("list-group-item");
+        item.textContent = "El paciente no ha registrado información.";
+
+        listadoSignosVitales.appendChild(item);
+      }
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
